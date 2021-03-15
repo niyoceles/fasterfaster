b9e5bb72a25d72c1e61862704047af44aff222f0
Celestin NIYONSABA <niyoceles3@gmail.com>
2021-03-15 14:59:47 +0200

    add typescript stuff

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -9,6 +9,26 @@ import ExploreContainer from '../../components/ExploreContainer';
 import './style.css';
 
 const Home: React.FC = () => {
+	let myString: string;
+	let myNumber: number;
+	let myAny: any;
+	let strArr: Array<string>;
+	let numberArr: Array<number>;
+	let strNumberTriple: [string, number];
+
+	strArr = ['strging name', '3'];
+	numberArr = [1, 3];
+	myAny = ['ddddd', 'ddddd', 1, true];
+	myString = 'kigali';
+	myNumber = 22;
+	strNumberTriple = ['kigali', 6666];
+
+	console.log('My results:', myString + myNumber);
+
+	console.log('Any:', myAny);
+	console.log('ArrayString:', strArr + '====' + numberArr);
+	console.log('StringNumberTriple:', strNumberTriple);
+	
 	return (
 		<IonPage>
 			<IonHeader>
